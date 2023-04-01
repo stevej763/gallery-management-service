c3b1a54d7e3f3880668a53037890cded595faab7
steve <steve.jones763@gmail.com>
2023-04-01 01:53:15 +0100

    Added integration test for find by Id

--- src/test/java/com/steve/gallery/gallerymanagementservice/integration/FindPhotoIntegrationTest.java
+++ src/test/java/com/steve/gallery/gallerymanagementservice/integration/FindPhotoIntegrationTest.java
@@ -1,10 +1,14 @@
 package com.steve.gallery.gallerymanagementservice.integration;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import com.steve.gallery.gallerymanagementservice.adapter.repository.mongo.MongoPhotoRepository;
 import com.steve.gallery.gallerymanagementservice.adapter.repository.mongo.PhotoDao;
 import com.steve.gallery.gallerymanagementservice.domain.Photo;
 import com.steve.gallery.gallerymanagementservice.domain.PhotoBuilder;
 import com.steve.gallery.gallerymanagementservice.domain.PhotoFinder;
+import java.util.List;
+import java.util.UUID;
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -12,37 +16,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-
 @SpringBootTest(classes = MongoTestConfiguration.class)
 public class FindPhotoIntegrationTest {
 
-    @Autowired
-    MongoTemplate testMongoTemplate;
+  @Autowired MongoTemplate testMongoTemplate;
 
-    private static final Photo PHOTO = new PhotoBuilder().build();
+  private static final UUID PHOTO_ID = UUID.randomUUID();
+  private static final Photo PHOTO = new PhotoBuilder().withPhotoId(PHOTO_ID).build();
 
-    @AfterEach
-    void tearDown() {
-        testMongoTemplate.getDb().drop();
-    }
+  @AfterEach
+  void tearDown() {
+    testMongoTemplate.getDb().drop();
+  }
 
-    @Test
-    public void canReturnAListOfPhotos() {
-        addPhotosToDb();
-        PhotoDao photoDao = new PhotoDao(testMongoTemplate);
-        MongoPhotoRepository photoRepository = new MongoPhotoRepository(photoDao);
-        PhotoFinder photoFinder = new PhotoFinder(photoRepository);
+  @Test
+  public void canReturnAListOfPhotos() {
+    addPhotosToDb();
+    PhotoDao photoDao = new PhotoDao(testMongoTemplate);
+    MongoPhotoRepository photoRepository = new MongoPhotoRepository(photoDao);
+    PhotoFinder photoFinder = new PhotoFinder(photoRepository);
 
-        List<Photo> result = photoFinder.findAll();
+    List<Photo> result = photoFinder.findAll();
 
-        List<Photo> photos = List.of(PHOTO);
-        assertThat(result, Is.is(photos));
-    }
+    List<Photo> photos = List.of(PHOTO);
+    assertThat(result, Is.is(photos));
+  }
 
-    private void addPhotosToDb() {
-        testMongoTemplate.save(PHOTO, "photos");
-    }
+  @Test
+  public void canReturnAPhotoById() {
+    addPhotosToDb();
+    PhotoDao photoDao = new PhotoDao(testMongoTemplate);
+    MongoPhotoRepository photoRepository = new MongoPhotoRepository(photoDao);
+    PhotoFinder photoFinder = new PhotoFinder(photoRepository);
+
+    Photo result = photoFinder.findPhotoById(PHOTO_ID);
+
+    assertThat(result, Is.is(PHOTO));
+  }
+
+  private void addPhotosToDb() {
+    testMongoTemplate.save(PHOTO, "photos");
+  }
 }
