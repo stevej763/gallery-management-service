4710eb5d276d7594b0784338fd0acc9904b28433
steve <steve.jones763@gmail.com>
2023-04-02 14:36:40 +0100

    removed workspace file from git and removed unused vars from ci-deploy script

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -12,15 +12,5 @@ docker run \
   --name ${CONTAINER_NAME} \
   -d \
   -p 8300:8100 \
-  -e SERVER_PORT=8100 \
-  -e MONGO_PORT=27017 \
-  -e MONGO_HOST=192.168.1.200 \
-  -e MONGO_REQUIRES_AUTH=true \
-  -e MONGO_USER=steve \
-  -e MONGO_PASSWORD=password \
-  -e MONGO_AUTH_DB=admin \
-  -e DATABASE_NAME=gallery-management-docker \
-  -e S3_HOSTNAME="http://192.168.1.200:9000" \
-  -e S3_BUCKET_NAME="gallery-docker" \
   steve763/gallery-manager:latest
 echo "container started"
